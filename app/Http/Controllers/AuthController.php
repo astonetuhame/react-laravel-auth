8ef137d9c3d67a173b0b4166af0ed3739c4d21d5
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 01:24:56 +0300

    Create register request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         $user = User::create([
             'first_name' => $request->input('first_name'),
