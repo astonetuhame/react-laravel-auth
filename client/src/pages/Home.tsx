ba5b8d2c645c0f0c56b37433e6c0d3b0595b1715
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 16:08:42 +0300

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+
 const Home = () => {
+
+    const[message, setMessage] = useState("")
+
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await axios.get('user')
+                    const user = response.data
+                 setMessage(`Hi ${user.first_name} ${user.last_name}`)
+                } catch (error) {
+                    setMessage('You are not logged in')
+                }
+               
+            }
+        )()
+    })
     return (
         <div className="container">
-            <h1>You are not logged in</h1>
+            <h1>{message}</h1>
         </div>
     )
 }
