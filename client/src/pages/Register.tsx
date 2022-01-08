f6389e83c9fe0810652107fdf6351c3322754575
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 15:43:13 +0300

    Update Register.tsx

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import { SyntheticEvent, useState } from "react"
 import axios from 'axios';
+import { Navigate } from "react-router-dom";
 
 const Register = () => {
 
@@ -8,11 +9,11 @@ const Register = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [passwordConfirm, setpasswordConfirm] = useState("")
-    const [errorMsg, setErrorMsg] = useState("")
+    const [redirect, setRedirect] = useState(false)
 
     const submit = async (e: SyntheticEvent) => {
- 
-        const response = await axios.post('http://localhost:8000/api/register', {
+        e.preventDefault();
+        const response = await axios.post('register', {
             first_name: firstName,
             last_name: lastName,
             email: email,
@@ -21,13 +22,18 @@ const Register = () => {
 
  
     
-        }   
+        }   )
 
-        
+        setRedirect(true)
  
     }
 
+    if (redirect) {
+        return <Navigate replace={true} to="/login" />
+    }
+
     return (
+       
            <main className="form-signin">
         <form onSubmit={submit}> 
             <h1 className="h3 mb-3 fw-normal">Please register </h1>
@@ -51,7 +57,6 @@ const Register = () => {
                 onChange={e=>setpasswordConfirm(e.target.value)}
                 
                 />
-                <p>{ errorMsg}</p>
  
             <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
         </form>
