1ea0ffe0b2696b072824ad555a861bede4eb34db
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 16:51:10 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from "react"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import axios from 'axios';
 
 function App() {
+
+  const [user, setUser] = useState(null)
+  
+   useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await axios.get('user')
+                  const user = response.data
+                  setUser(user)
+                } catch (error) {
+                  setUser(null)
+                }
+               
+            }
+        )()
+   },[])
+  
+  
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav />
+      <Nav user={ user}/>
         
         <Routes>
-        <Route path="/"  element={<Home />} />
+          <Route path="/" element={() => <Home user={ user}/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
