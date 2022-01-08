58f1e7b651578f043446916149aec9ef34e798ee
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 03:25:58 +0300

    Create reset password api

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\PasswordResetRequest;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PasswordController extends Controller
 {
@@ -27,6 +31,21 @@ class PasswordController extends Controller
 
         return response([
             'message' => 'Check your email'
+        ]);
+    }
+
+    public function reset(PasswordResetRequest $request)
+    {
+        $passwordReset = DB::table('password_resets')->where('token', $request->input('token'))->first();
+        if (!$user = User::where('email', $passwordReset->email)->first()) {
+            throw new NotFoundHttpException('User not found');
+        }
+
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return response([
+            'message' => 'Success'
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('forgot', [PasswordController::class, 'forgot']);
+Route::post('reset', [PasswordController::class, 'reset']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
