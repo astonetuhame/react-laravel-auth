8d1e7609c532b0c48d595ebfc01bda9d5e21db2a
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 15:08:57 +0300

    Update Register.tsx

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,18 +1,59 @@
+import { SyntheticEvent, useState } from "react"
+import axios from 'axios';
+
 const Register = () => {
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirm, setpasswordConfirm] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
+
+    const submit = async (e: SyntheticEvent) => {
+ 
+        const response = await axios.post('http://localhost:8000/api/register', {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: password,
+            password_confirm: passwordConfirm
+
+ 
+    
+        }   
+
+        
+ 
+    }
+
     return (
            <main className="form-signin">
-        <form>
+        <form onSubmit={submit}> 
             <h1 className="h3 mb-3 fw-normal">Please register </h1>
 
-                <input type="text" className="form-control mb-2"  placeholder="First Name" required/>
-                <input type="text" className="form-control mb-2"  placeholder="Last Name" required/>
-
-
-                <input type="email" className="form-control mb-2"  placeholder="email" required/>
-                <input type="password" className="form-control mb-2"  placeholder="Password" required/>
-                <input type="password" className="form-control mb-2"  placeholder="Password Confirm" required/>
+                <input type="text" className="form-control mb-2" placeholder="First Name" required
+                onChange={e=>setFirstName(e.target.value)}
+                />
+                <input type="text" className="form-control mb-2" placeholder="Last Name" required
+                onChange={e=>setLastName(e.target.value)}
+                
+                />
+                <input type="email" className="form-control mb-2" placeholder="email" required
+                onChange={e=>setEmail(e.target.value)}
+                
+                />
+                <input type="password" className="form-control mb-2" placeholder="Password" required
+                onChange={e=>setPassword(e.target.value)}
+                
+                />
+                <input type="password" className="form-control mb-2" placeholder="Password Confirm" required
+                onChange={e=>setpasswordConfirm(e.target.value)}
+                
+                />
+                <p>{ errorMsg}</p>
  
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
         </form>
     </main>
     )
