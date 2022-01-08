35c501b3a96632cab7185a3cbeedb71114d1f964
Astone Tuhame <astonetuhame@gmail.com>
2022-01-08 16:51:05 +0300

    Update Nav.tsx

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,6 +1,30 @@
 import { Link } from "react-router-dom"
+import axios from 'axios';
 
-const Nav = () => {
+const Nav = ({user}:{user:any}) => {
+
+    const logout = async () => {
+         await axios.get('logout', {})
+    }
+
+    let links;
+    if(user){
+        links = (<ul className="navbar-nav mx-auto ">
+                <li className="nav-item">
+                <Link to="/ " onClick={logout } className="nav-link">Logout</Link>
+                </li>
+                
+            </ul>)
+    } else {
+        links = (<ul className="navbar-nav mx-auto ">
+                <li className="nav-item">
+                    <Link to="/login" className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/register" className="nav-link">Register</Link>
+                </li>
+            </ul>)
+    }
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <ul className="navbar-nav mr-auto">
@@ -8,14 +32,7 @@ const Nav = () => {
                     <Link to="/" className="nav-link">Home</Link>
                 </li>
             </ul>
-            <ul className="navbar-nav mx-auto ">
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/register" className="nav-link">Register</Link>
-                </li>
-            </ul>
+            {links}
         </nav>
     )
 }
